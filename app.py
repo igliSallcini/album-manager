2bd177a1e10cd436dd97e689dc35961d02c62953
igliSallcini <iglisallcini@gmail.com>
2022-10-05 19:19:54 +0200

    test

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #Krijimi i modeleve
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), unique = True, nullable=False)
 
